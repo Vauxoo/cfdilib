701a42aec6718c5da9ab0283f90eb0d916a9fb16
Nhomar Hernández <nhomar@vauxoo.com>
2016-02-09 08:25:34 -0600

    [IMP] Testing the validate method. [IMP] Structure of setters minimal in order to have the correct approach on classes. [IMP] template, schema working like a charm

--- tests/test_cfdilib.py
+++ tests/test_cfdilib.py
@@ -8,9 +8,11 @@ test_cfdilib
 Tests for `cfdilib` module.
 """
 
+from os.path import join, dirname
 import unittest
 
-from cfdilib import cfdilib
+import cfdilib
+from cfdilib.cfdilib import Invoice32
 
 
 class TestCfdilib(unittest.TestCase):
@@ -21,8 +23,19 @@ class TestCfdilib(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_000_something(self):
-        pass
+    def _get_test_file(self, file_name):
+        test_data_dir = join(dirname(cfdilib.__file__), "..", "tests", "demo")
+        test_data_file = join(test_data_dir, file_name)
+        with open(test_data_file) as data_file:
+            return data_file.read()
+
+    def test_000_validator(self):
+        """Given a known valid/invalid file asserting with a known xsd"""
+        # Force return a know valid xml file.
+
+        invoice = Invoice32({})
+        self.assertTrue(invoice.validate(invoice.schema,
+                                         self._get_test_file('cfdv32.xml')))
 
 
 if __name__ == '__main__':
