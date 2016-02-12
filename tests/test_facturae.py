104090add04320395927e77d77f695518ade1996
Nhomar Hernandez <nhomar@gmail.com>
2016-02-12 01:31:56 -0600

    [IMP] Just copying tests this are the lines you will need to replicate on your test. [note] may be this is better if we use only one file for tests but this method is to be more didactic than other thing.

--- tests/test_facturae.py
+++ tests/test_facturae.py
@@ -11,10 +11,10 @@ Tests for `cfdilib` module.
 from os.path import join, dirname
 import unittest
 
-from cfdilib import cfdilib, cfdv32
+from cfdilib import cfdilib, facturaev321
 
 
-class TestCfdilib(unittest.TestCase):
+class TestFacturaev321(unittest.TestCase):
 
     def _get_test_file(self, file_name):
         test_data_file = join(self.test_data_dir, file_name)
@@ -23,34 +23,34 @@ class TestCfdilib(unittest.TestCase):
 
     def setUp(self):
         self.test_data_dir = join(dirname(cfdilib.__file__), "..", "tests", "demo")
-        self.dict_invoice_basic_32 = eval(self._get_test_file('basic_invoice_32.txt'))
-        self.dict_invoice_basic_32_errored = eval(self._get_test_file('basic_invoice_32_errored.txt'))
+        self.dict_invoice_basic_32 = eval(self._get_test_file('basic_facturae_v321.txt'))
+        self.dict_invoice_basic_32_errored = eval(self._get_test_file('basic_facturae_v321_errored.txt'))
 
     def tearDown(self):
         pass
 
     def test_001_get_xsd_documentation(self):
         '''Getting a documentation from a given Clark's Notated xsd element'''
-        invoice = cfdv32.get_invoice(self.dict_invoice_basic_32)
+        invoice = facturaev321.get_invoice(self.dict_invoice_basic_32)
         self.assertTrue(invoice.get_documentation('{http://www.sat.gob.mx/cfd/3}Impuestos')
                         .find('impuestos aplicables') > 0,
                         'Documentation did not returns the expected element')
 
     def test_002_get_cfd_debugged(self):
         '''validate that with a given valid dict an invoice object is created in debug_mode'''
-        invoice = cfdv32.get_invoice(self.dict_invoice_basic_32, debug_mode=True)
+        invoice = facturaev321.get_invoice(self.dict_invoice_basic_32, debug_mode=True)
         self.assertTrue(invoice.document,
                         'A valid dictionary gave error debugged_mode enabled gave an error.')
 
     def test_003_get_cfd(self):
         '''validate that with a given valid dict an invoice object is created in debug_mode'''
-        invoice = cfdv32.get_invoice(self.dict_invoice_basic_32, debug_mode=False)
+        invoice = facturaev321.get_invoice(self.dict_invoice_basic_32, debug_mode=False)
         self.assertTrue(invoice.document,
                         'A valid dictionary gave error an error')
 
     def test_004_get_errored(self):
         '''validate that with a given invalid dict raise properly errors on ups object'''
-        invoice = cfdv32.get_invoice(self.dict_invoice_basic_32_errored)
+        invoice = facturaev321.get_invoice(self.dict_invoice_basic_32_errored)
 
         self.assertTrue(invoice.ups,
                       'An invalid dictionary gave a valid output, that is wrong.')
