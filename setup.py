0cd52ed3156397a39e4ca53f07334c1705391b23
Nhomar Hernández <nhomar@vauxoo.com>
2016-07-25 16:51:42 -0500

    Bump version: 0.1.6 → 0.1.7

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='cfdilib',
-    version='0.1.6',
+    version='0.1.7',
     description="Library to manage xml CDFI from " +
     "python and sign with several pacs.",
     long_description=readme + '\n\n' + history,
