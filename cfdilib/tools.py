0c33ab5e0febe434f1ab301c9545941fd37e3f1a
Nhomar Hernández <nhomar@vauxoo.com>
2016-07-25 01:26:21 -0500

    [IMP] Added retry logic to download files.
    Due to some network problem the url's used can fail then added a pretty
    generic retry decorator (RTFD to know where I copied from).
    Retry

--- cfdilib/tools.py
+++ cfdilib/tools.py
@@ -3,7 +3,53 @@ import urllib2
 from tempfile import NamedTemporaryFile
 from lxml import etree
 import re
+import time
+from functools import wraps
 
+
+def retry(exception_to_check, tries=4, delay=3, back_off=2, logger=None):
+    """Retry calling the decorated function using an exponential backoff.
+
+    http://www.saltycrane.com/blog/2009/11/trying-out-retry-decorator-python/
+    original from: http://wiki.python.org/moin/PythonDecoratorLibrary#Retry
+
+    :param exception_to_check: the exception to check. may be a tuple of
+        exceptions to check
+    :type exception_to_check: Exception or tuple
+    :param tries: number of times to try (not retry) before giving up
+    :type tries: int
+    :param delay: initial delay between retries in seconds
+    :type delay: int
+    :param back_off: back_off multiplier e.g. value of 2 will double the
+        delay each retry
+    :type back_off: int
+    :param logger: logger to use. If None, print
+    :type logger: logging.Logger instance
+    """
+
+    def deco_retry(f):
+
+        @wraps(f)
+        def f_retry(*args, **kwargs):
+            mtries, mdelay = tries, delay
+            while mtries > 1:
+                try:
+                    return f(*args, **kwargs)
+                except exception_to_check, e:
+                    msg = "%s, Retrying in %d seconds..." % (str(e),
+                                                             mdelay)
+                    if logger:
+                        logger.warning(msg)
+                    else:
+                        print msg
+                    time.sleep(mdelay)
+                    mtries -= 1
+                    mdelay *= back_off
+            return f(*args, **kwargs)
+
+        return f_retry  # true decorator
+
+    return deco_retry
 
 class Tools(object):
 
@@ -12,6 +58,7 @@ class Tools(object):
         return element_name.startswith('http')
 
     @staticmethod
+    @retry(urllib2.URLError, tries=3, delay=3, back_off=2)
     def cache_it(url):
         """Take an url which deliver a plain document
         and convert it to a temporary file
@@ -54,5 +101,6 @@ class Tools(object):
         transform = etree.XSLT(xslt)
         newdom = transform(dom)
         return newdom
+
 
 tools = Tools()
