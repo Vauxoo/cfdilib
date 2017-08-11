499f51dc10c166c6b185faa6ba99f158e3da64ff
goliveirab <germana@vauxoo.com>
2017-08-11 14:14:51 +0000

    [REF] cfdilib: Fixed comprobante to payroll12.xml
    The SAT documentation to complement to payroll indicate that the
    xsi:schemaLocation must be defined in the Comprobante, and not in the
    Complement. By this reason was completed the template to fill the CFDI
    and complement to payroll in the same document.
    
    Adapted test to new changes.

--- cfdilib/cfdv33.py
+++ cfdilib/cfdv33.py
@@ -35,3 +35,38 @@ class CFDv33(BaseDocument):
 
 def get_cfdi(dict_invoice, debug_mode=False):
     return CFDv33(dict_invoice, debug_mode=debug_mode)
+
+
+class Payroll12(BaseDocument):
+    """Invoice document to comply with
+    cfdi: v3.3 for Invoice Mexico Standards."""
+
+    def __init__(self, dict_invoice, debug_mode=False):
+        self.template_fname = 'payroll12.xml'
+        # We explicitly cached into s3 with the local test then ensure use
+        # the s3 url to use our cache. remove the tools.s3_url if you want to
+        # load first in the future
+        # TODO - Change by reference to payroll v1.2
+        self.xslt_fname = tools.s3_url(
+            'http://s3.vauxoo.com/sitio_internet/cfd/3/cadenaoriginal_3_2/cadenaoriginal_3_2.xslt')
+        self.global_namespace = 'http://www.sat.gob.mx/sitio_internet/cfd'
+        self.set_template(self.template_fname)
+        super(Payroll12, self).__init__(dict_invoice, debug_mode=debug_mode)
+        # This method must be called from the inherited
+        #   __init__ always AFTER the super.
+
+    def set_template(self, template_fname):
+        self.template = super(Payroll12, self).set_template(template_fname)
+
+    def set_schema(self, schema_fname):
+        schema_fname = 'cfdv33.xsd'
+        self.schema = super(Payroll12, self).set_schema(schema_fname)
+
+    def set_xslt(self):
+        # TODO: Standarize the schema in this way also,
+        #       we can not use different algorithms here
+        self.xstl = super(Payroll12, self).set_xslt()
+
+
+def get_payroll(dict_payroll, debug_mode=False):
+    return Payroll12(dict_payroll, debug_mode=debug_mode)
