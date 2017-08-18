66cfddb40a5e21480560ab63767dd4cbf66d1913
Nhomar Hernandez <nhomar@vauxoo.com>
2017-08-17 23:00:36 -0500

    Bump version: 0.5.2 → 0.5.3

--- cfdilib/__init__.py
+++ cfdilib/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = 'Vauxoo OpenSource Specialists.'
 __email__ = 'mexico@vauxoo.com'
-__version__ = '0.5.2'
+__version__ = '0.5.3'
--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ test_requirements = [
 
 setup(
     name='cfdilib',
-    version='0.5.2',
+    version='0.5.3',
     description="Library to manage xml CDFI from " +
     "python and sign with several pacs.",
     long_description=readme + '\n\n' + history,
